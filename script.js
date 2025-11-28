093cbe35ffc17d8a6d6be9ad451052d0d1ae5021
Giovanna Pereira Candido de Souza <36930163+Giovannapcs@users.noreply.github.com>
2025-11-28 16:53:15 -0300

    Refactor login and cadastro functions with comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/* Código JavaScript para o arquivo 'script.js' */
 
-[cite_start]// FUNÇÃO VOLTAR AO TOPO [cite: 877]
+
+[cite_start]
 function topo() {
-    [cite_start]// Rola a janela suavemente até o topo (posição 0, 0) [cite: 878]
+    [cite_start]
     window.scrollTo({
         top: 0,
         left: 0,
@@ -11,35 +11,35 @@ function topo() {
 }
 
 
-[cite_start]// FUNÇÃO VALIDAÇÃO DE LOGIN [cite: 892]
+[cite_start]
 function login(){
-    var logado = 0; [cite_start]// Inicia a variável de controle [cite: 906]
+    var logado = 0; [cite_start]
     
-    [cite_start]// Pega o valor do input com 'name="usuario"' [cite: 907]
+    [cite_start]
     var usuario = document.getElementsByName('usuario')[0].value;
-    usuario = usuario.toLowerCase(); [cite_start]// Converte para minúsculas [cite: 909]
+    usuario = usuario.toLowerCase(); [cite_start]
     
-    [cite_start]// Pega o valor do input com 'id="senha"' [cite: 908]
+    [cite_start]
     var senha = document.getElementById('senha').value;
-    senha = senha.toLowerCase(); [cite_start]// Converte para minúsculas [cite: 909]
+    senha = senha.toLowerCase(); [cite_start]
     
-    [cite_start]// Verifica as credenciais [cite: 897]
+    [cite_start]
     if(usuario == "admin" && senha == "123456"){
-        [cite_start]// Credenciais corretas: Redireciona para a home [cite: 899]
+        [cite_start]
         window.location = "index.html"; 
-        [cite_start]logado = 1 // Altera a variável para 1 [cite: 900]
+        [cite_start]logado = 1
     }
     
-    [cite_start]// Verifica se não logou (logado == 0) [cite: 901]
+    [cite_start]
     if(logado == 0) {
-        [cite_start]// Credenciais incorretas: Exibe alerta [cite: 903]
+        [cite_start]
         alert("Acesso Negado. Dados incorretos");
     }
 }
 
 
-[cite_start]// FUNÇÃO CADASTRO (Apenas para emitir alerta e redirecionar) [cite: 962]
+[cite_start]
 function cadastro() {
-    alert("Cadastrado com sucesso!"); [cite_start]// Alerta de sucesso [cite: 963]
-    window.location.href = "index.html"; [cite_start]// Redireciona para a home [cite: 963]
+    alert("Cadastrado com sucesso!"); [cite_start]
+    window.location.href = "index.html"; [cite_start]
 }
